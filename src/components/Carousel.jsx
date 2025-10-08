7b828477950a022cb750d2ea4ee786abba0a1313
gdrmvrm <gdrmvrm@gmail.com>
2025-10-08 12:35:34 +0100

    Render person info

---
+++ src/components/Carousel.jsx
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+import { FaQuoteRight } from 'react-icons/fa';
+import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
+import { shortList, list, longList } from '../data';
+
+const Carousel = () => {
+  const [people, setPeople] = useState(shortList);
+
+  return (
+    <section className="slider-container">
+      {people.map((person) => {
+        const { id, image, name, title, quote } = person;
+        return (
+          <article className="slide" key={id}>
+            <img src={image} alt={name} className="person-img" />
+            <h5 className="name">{name}</h5>
+            <p className="title">{title}</p>
+            <p className="text">{quote}</p>
+            <FaQuoteRight className="icon" />
+          </article>
+        );
+      })}
+      
+    </section>
+  );
+};
+
+export default Carousel;
